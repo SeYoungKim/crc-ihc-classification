60c0480f50db4d775606aff9f6e78a7e9d066e2d
anne trinh <anne.trinh@cruk.cam.ac.uk>
2016-02-09 23:34:53 -0500

    update classifier

--- PathologistBasedClassifier/server.R
+++ PathologistBasedClassifier/server.R
@@ -5,32 +5,21 @@ load("classifier.RData")
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-  
-  ## Note: should probably group low and moderate staining samples together?
-  datasetInput <- reactive({
+   datasetInput <- reactive({
     a1=data.frame(CDX2_StainInt=input$CDX2, #levels=c("low","mod", "peak")),
                HTR2B_StainInt=input$HTR2B, #levels=c("low", "low", "mod")),
                ZEB1_StAreaFrac.norm=input$ZEB1, #factor(input$ZEB1, levels=c("low", "mod")),
-               KER_StainInt=input$KERS, #factor(input$KERS, levels=c("low","mod", "peak")),
+               KER_StainInt=input$KERS, 
               CDX2_StAreaFrac=input$CDX2C*input$KERC/100,
-              #factor(cut(input$CDX2C*input$KERC/100, c(-1, 10, 30, 105) , levels=c("low", "mod", "peak"))),
+              FRMD6_StainInt=input$FRMD6I, #factor(input$ZEB1, levels=c("low", "mod")),
               CDX2_StAreaFrac.norm=input$CDX2C,
-              #factor(cut(input$CDX2C, c(-1, 50, 70, 101), levels=c("low", "mod", "peak"))),
-              FRMD6_StAreaFrac=input$FRMD6C*input$KERC/100, #factor(cut(input$FRMD6C*input$KERC/100, c(-1, 45, 101), levels=c("low", "mod"))),
-              FRMD6_StAreaFrac.norm=input$FRMD6C, #factor(cut(input$FRMD6C, c(-1, 70, 101), levels=c("low", "mod")))
-              KER_StAreaFrac=input$KERC #, levels=c("low", "mod"))
+              FRMD6_StAreaFrac=input$FRMD6C*input$KERC/100, 
+               FRMD6_StAreaFrac.norm=input$FRMD6C, 
+                             KER_StAreaFrac=input$KERC
                       )
-   # browser()
     levels(a1$CDX2_StainInt)=list("low"="low", "mod"="mod", "peak"="high")
-   # browser()
     levels(a1$HTR2B_StainInt)=list("low"="low", "low"="mod", "mod"="high")
+   levels(a1$FRMD6_StainInt)=list("low"="low", "mod"="mod", "mod"="high")
     levels(a1$KER_StainInt)=list("low"="low", "mod"="mod", "peak"="high")
     levels(a1$ZEB1_StAreaFrac.norm)=list("low"="absent", "mod"="present")
     a1$CDX2_StAreaFrac=ifelse(a1$CDX2_StAreaFrac<5, "low", ifelse(a1$CDX2_StAreaFrac>20, "peak", "mod"))
@@ -38,12 +27,11 @@ shinyServer(function(input, output) {
     a1$CDX2_StAreaFrac.norm=ifelse(a1$CDX2_StAreaFrac.norm<30, "low", ifelse(a1$CDX2_StAreaFrac.norm>50, "peak", "mod"))
     a1$CDX2_StAreaFrac.norm=factor(a1$CDX2_StAreaFrac.norm, levels=c("low", "mod", "peak"))
     
-    a1$FRMD6_StAreaFrac=ifelse(a1$FRMD6_StAreaFrac<45, "low", "mod")
-    a1$FRMD6_StAreaFrac=factor(a1$FRMD6_StAreaFrac, levels=c("low", "mod"))
+   a1$FRMD6_StAreaFrac=ifelse(a1$FRMD6_StAreaFrac<5, "low", ifelse(a1$FRMD6_StAreaFrac>40, "peak", "mod"))
+    a1$FRMD6_StAreaFrac=factor(a1$FRMD6_StAreaFrac, levels=c("low", "mod", "peak"))
     a1$FRMD6_StAreaFrac.norm=ifelse(a1$FRMD6_StAreaFrac.norm<70, "low", "mod")
-    a1$FRMD6_StAreaFrac.norm=factor(a1$FRMD6_StAreaFrac.norm, levels=c("low", "mod"))
-    a1$KER_StAreaFrac=ifelse(a1$KER_StAreaFrac<40, "low", "mod")
-    #browser()
+    a1$FRMD6_StAreaFrac.norm=factor(a1$FRMD6_StAreaFrac.norm, levels=c("low", "mod")) 
+   a1$KER_StAreaFrac=ifelse(a1$KER_StAreaFrac<40, "low", "mod")
     a1$KER_StAreaFrac=factor(a1$KER_StAreaFrac, levels=c("low", "mod"))
     a1
     })
@@ -57,12 +45,7 @@ shinyServer(function(input, output) {
   
   
   output$Classification <-renderUI({
-  #  browser()
     ClassRslt=predict(rf.test, datasetInput(), type="prob")
-    #output$CCS2=ClassRslt[3]
-    #ClassRslt
-    #sprintf(paste("\n Probability of epithelial-like subtype: ", ClassRslt[1],
-  #          "\n Probability of mesenchymal-like subtype:",  ClassRslt[2]))
     str1 <- paste("\n Probability of epithelial-like subtype: ", ClassRslt[1])
     str2 <- paste("\n Probability of mesenchymal-like subtype:",  ClassRslt[2])
     HTML(paste(str1, str2, sep = '<br/>'))
@@ -73,4 +56,10 @@ shinyServer(function(input, output) {
     head(datasetInput())
   })
   
+  output$Nplot <-renderPlot({
+    ClassRslt=predict(rf.test, datasetInput(), type="prob")
+    barplot(t(ClassRslt), horiz=T, beside=F, col=c("lightblue", "darkblue"), names.arg=" ",
+            legend.text=c("epithelial-like", "mesenchymal-like"), xlab="prediction probability")
+    }
+    )
 })
--- PathologistBasedClassifier/ui.R
+++ PathologistBasedClassifier/ui.R
@@ -8,46 +8,57 @@ shinyUI(fluidPage(
   
   # Sidebar with a slider input for the number of bins
   sidebarLayout(
-    mainPanel(
+    sidebarPanel(
       selectInput("CDX2", "CDX2 Stain Intensity", 
                   choices = c("low","mod", "high")),
-     # selectInput("CDX2A", "CDX2 Epithelial content", 
-    #              choices = c("low", "mod", "peak")),
-     # selectInput("FRMD6", "FRMD6 Epithelial content", 
-    #              choices = c("low", "mod", "high")),
       selectInput("HTR2B", "HTR2B Stain Intensity", 
                   choices = c("low", "mod", "high")),
+    selectInput("FRMD6I", "FRMD6 Stain Intensity", 
+                choices = c("low", "mod", "high")),
       selectInput("ZEB1", "ZEB1 Epithelial content", 
                   choices = c("absent", "present")),
       selectInput("KERS", "KER Stain Intensity", 
-                  choices = c("low","mod", "peak")),
+                  choices = c("low","mod", "high")),
     sliderInput("CDX2C", "CDX2 content", 0, 100,
-                50, 5),
+                0, 5),
     sliderInput("FRMD6C", "FRMD6 content", 0, 100,
                 50, 5),
     sliderInput("KERC", "Keratin content", 0, 100,
                 50, 5),
-    #  selectInput("KERA", "KER Stain Content", 
-    #              choices = c("low", "mod")),
-    #  helpText("Note: while the data view will show only the specified",
-    #           "number of observations, the summary will still be based",
-    #           "on the full dataset."),
       submitButton()
     ),
   
     
       
-    # Show a plot of the generated distribution
-    sidebarPanel(
-    # h3(textOutput("caption", container = span)),
-   #  print("Input data:"),
-    # verbatimTextOutput("summary"), 
-   #  tableOutput("view"),
-    h3("Classification result:"),
-    h4(htmlOutput("Classification"))
-    #h4(sprintf("Probability of mesenchymal-like phenotype: %s")),
-   # tableOutput("Classification")
-    )
+   mainPanel(
+   tabsetPanel(
+     tabPanel("Compute Classification", h3("Classification result:"),
+              h4(htmlOutput("Classification")),
+              plotOutput("Nplot")),
+     tabPanel("CDX2", h4("Examples of CDX2 scoring:"),
+              img(src="cdx2_content.jpg"),
+              h5("Examples of epithelial specific stain content: 1. 25% 2. 55% 3. 95%"),
+              img(src="cdx2_intensity.jpg"),
+              h5("Examples of 1. Low (Left), 2. Medium (Middle), 3. High (Right)  intensity staining ")
+               ), 
+     tabPanel("FRMD6", h4("Examples of FRMD6 scoring:"),
+              img(src="frmd6_content.jpg"),
+              h5("Examples of epithelial specific stain content: 1. 10% 2. 60% 3. 90%"),
+              img(src="frmd6_intensity.jpg"),
+              h5("Examples of 1. Low (Left), 2. Medium (Middle), 3. High (Right)  intensity staining ")),
+     tabPanel("HTR2B", h4("Examples of HTR2B scoring:"),
+              img(src="htr2b_intensity.jpg"),
+              h5("Examples of 1. Low (Left), 2. Medium (Middle), 3. High (Right)  intensity staining ")),
+     tabPanel("ZEB1", h4("Examples of ZEB1 scoring:"),
+              img(src="zeb1_intensity.jpg"),
+              h5("Examples of 1. Absent (Left) and 2. Epithelial specific scoring (Right")),
+     tabPanel("KER", h4("Examples of KER scoring:"),
+              img(src="ker_content.jpg"),
+              h5("Examples of keratin content within a core: 1. 15% 2. 50% 3. 85%"),
+              img(src="ker_intensity.jpg"),
+              h5("Examples of 1. Low (Left), 2. Medium (Middle), 3. High (Right)  intensity staining ")
+             )
+   ))
   )
 ))
 
